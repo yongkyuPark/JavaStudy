d3af47396ad9ebaf0be7ace27708aa6cb30d6683
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-07 00:40:32 +0900

    feat : ReadLock & WriteLock

---
+++ Java-Concurrency-Programming/src/chapter08/exam03/BankAccount.java
@@ -0,0 +1,62 @@
+package chapter08.exam03;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class BankAccount {
+
+    private final ReadWriteLock lock;
+    private Map<String, Integer> balance;
+
+    public BankAccount(ReadWriteLock lock, int amount) {
+        this.lock = lock;
+        balance = new HashMap<>();
+        balance.put("account1", amount);
+    }
+
+    public int getBalance() {
+        lock.readLock().lock();
+        try {
+            Thread.sleep(1000);
+            return balance.get("account1");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public void deposit(int amount) {
+        lock.writeLock().lock();
+        try {
+            Thread.sleep(2000);
+            int currentBalance = balance.get("account1");
+            currentBalance += amount;
+            balance.put("account1", currentBalance);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public void withdraw(int amount) {
+        lock.writeLock().lock();
+        try {
+            int currentBalance = balance.get("account1");
+            if (currentBalance >= amount) {
+                currentBalance -= amount;
+                balance.put("account1", currentBalance);
+                System.out.println(Thread.currentThread().getName() + " - 출금 성공");
+            } else {
+                System.out.println(Thread.currentThread().getName() + " - 출금 실패, 잔액 부족");
+            }
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+}
---
+++ Java-Concurrency-Programming/src/chapter08/exam03/ReadWriteLockReadLockExample.java
@@ -0,0 +1,41 @@
+package chapter08.exam03;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockReadLockExample {
+
+    public static void main(String[] args) {
+
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+
+        BankAccount account = new BankAccount(lock, 0);
+
+        // 읽기 스레드가 잔액 조회
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                int balance = account.getBalance();
+                System.out.println(Thread.currentThread().getName() + " - 현재 잔액: " + balance);
+            }).start();
+        }
+
+        // 쓰기 스레드가 입금
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                int depositAmount = (int) (Math.random() * 1000);
+                account.deposit(depositAmount);
+                System.out.println(Thread.currentThread().getName() + " - 입금: " + depositAmount);
+            }).start();
+        }
+
+        // 읽기 스레드가 잔액 조회
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                int balance = account.getBalance();
+                System.out.println(Thread.currentThread().getName() + " - 현재 잔액: " + balance);
+            }).start();
+        }
+
+    }
+
+}
