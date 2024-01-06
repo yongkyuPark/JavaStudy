75092df4095e1a3b7668cca6176d172c2d707672
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-06 23:56:52 +0900

    feat : ReadWriteLock & ReentrantReadWriteLock 학습

---
+++ Java-Concurrency-Programming/src/chapter08/exam03/ReadWriteLockExample.java
@@ -0,0 +1,71 @@
+package chapter08.exam03;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockExample {
+    public static void main(String[] args) {
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        SharedData sharedData = new SharedData();
+
+        Thread reader1 = new Thread(() -> {
+            readWriteLock.readLock().lock();
+            try {
+                System.out.println("읽기 스레드 1이 데이터를 읽고 있습니다. 데이터: " + sharedData.getData());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        });
+
+        Thread reader2 = new Thread(() -> {
+            readWriteLock.readLock().lock();
+            try {
+                System.out.println("읽기 스레드 2가 데이터를 읽고 있습니다. 데이터: " + sharedData.getData());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        });
+
+        Thread writer = new Thread(() -> {
+            readWriteLock.writeLock().lock();
+            try {
+                System.out.println("쓰기 스레드가 데이터를 쓰고 있습니다");
+                sharedData.setData(40);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("쓰기 스레드가 데이터를 변경 했습니다. 데이터: " + sharedData.getData());
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        });
+
+        writer.start();
+        reader1.start();
+        reader2.start();
+    }
+    static class SharedData{
+        private int data = 0;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+    }
+
+}
