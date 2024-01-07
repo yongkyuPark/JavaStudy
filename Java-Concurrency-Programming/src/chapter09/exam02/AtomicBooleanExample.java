8a4a5139d38557039ae5550935cdbec89aeb0c30
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-07 18:46:51 +0900

    feat : Atomic Variables - 단일연산변수 학습

---
+++ Java-Concurrency-Programming/src/chapter09/exam02/AtomicBooleanExample.java
@@ -0,0 +1,44 @@
+package chapter09.exam02;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class AtomicBooleanExample {
+
+    private static AtomicBoolean flag = new AtomicBoolean(false);
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                if (flag.compareAndSet(false, true)) {
+                    System.out.println("스레드 1 이 바쁜 대기 중..." + flag.get());
+                }
+                System.out.println(" 스레드 1 이 임계영역 수행 중..");
+                flag.set(false);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                while (!flag.compareAndSet(false, true)) {
+                    System.out.println("스레드 2 가 바쁜 대기 중..." + flag.get());
+                }
+                System.out.println(" 스레드 2 가 임계영역 수행 중..");
+                flag.set(false);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
