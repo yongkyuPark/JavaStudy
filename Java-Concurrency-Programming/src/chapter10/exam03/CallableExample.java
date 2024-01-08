823ebcb43682104ef4f826b6e3809da98372244c
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-08 20:51:01 +0900

    feat : Runnable and Callable 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam03/CallableExample.java
@@ -0,0 +1,32 @@
+package chapter10.exam03;
+
+import java.util.concurrent.*;
+
+public class CallableExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Callable<Integer> callableTask = () -> {
+            System.out.println("Callable 작업 수행중..");
+            System.out.println("Callable 작업 완료");
+
+            return 42;
+        };
+
+        Future<Integer> future = executorService.submit(callableTask);
+        int result;
+        try {
+            result = future.get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Callable 작업 결과 : " + result);
+
+        executorService.shutdown();
+
+    }
+}
---
+++ Java-Concurrency-Programming/src/chapter10/exam03/RunnableExample.java
@@ -0,0 +1,20 @@
+package chapter10.exam03;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RunnableExample {
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Runnable runnableTask = () ->{
+            System.out.println("Runnable 작업 수행 중..");
+            System.out.println("Runnable 작업 완료");
+        };
+
+        executorService.execute(runnableTask);
+
+        executorService.shutdown();
+    }
+}
