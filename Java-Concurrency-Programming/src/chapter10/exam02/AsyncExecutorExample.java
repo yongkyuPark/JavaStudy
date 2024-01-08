f7d7a96dd0496cb0370d7b6315b91edbf859c14e
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-08 20:31:01 +0900

    feat : Executor 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam02/AsyncExecutorExample.java
@@ -0,0 +1,32 @@
+package chapter10.exam02;
+
+import java.util.concurrent.Executor;
+
+public class AsyncExecutorExample {
+
+    public static void main(String[] args) {
+        Executor asyncExecutor = new AsyncExecutor();
+
+        asyncExecutor.execute(() -> {
+            System.out.println("비동기 작업 1 수행 중...");
+            // 작업 수행
+            System.out.println("비동기 작업 1 완료...");
+        });
+
+        asyncExecutor.execute(() -> {
+            System.out.println("비동기 작업 2 수행 중...");
+            // 작업 수행
+            System.out.println("비동기 작업 2 완료...");
+        });
+    }
+
+    static class AsyncExecutor implements Executor {
+
+        @Override
+        public void execute(Runnable command) {
+            Thread thread = new Thread(command);
+            thread.start();
+        }
+    }
+
+}
---
+++ Java-Concurrency-Programming/src/chapter10/exam02/SyncExecutorExample.java
@@ -0,0 +1,30 @@
+package chapter10.exam02;
+
+import java.util.concurrent.Executor;
+
+public class SyncExecutorExample {
+
+    public static void main(String[] args) {
+        Executor syncExecutor = new SyncExecutor();
+
+        syncExecutor.execute(() -> {
+            System.out.println("동기 작업 1 수행 중...");
+            // 작업 수행
+            System.out.println("동기 작업 1 완료...");
+        });
+
+        syncExecutor.execute(() -> {
+            System.out.println("동기 작업 2 수행 중...");
+            // 작업 수행
+            System.out.println("동기 작업 2 완료...");
+        });
+    }
+
+    static class SyncExecutor implements Executor {
+
+        @Override
+        public void execute(Runnable command) {
+            command.run();
+        }
+    }
+}
