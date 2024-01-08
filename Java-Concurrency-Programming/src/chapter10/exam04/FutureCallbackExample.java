0d2d47e9bf123100834d33d4966df0769e06ae59
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-08 21:22:36 +0900

    feat : Future & Callback 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam04/FutureCallbackExample.java
@@ -0,0 +1,44 @@
+package chapter10.exam04;
+
+import java.util.concurrent.*;
+
+public class FutureCallbackExample {
+
+    interface Callback {
+        void onComplete(int result);
+    }
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Callable<Integer> callableTask = () -> {
+            Thread.sleep(1000);
+            return 42;
+        };
+
+        Future<Integer> future = executorService.submit(callableTask);
+        System.out.println("비동기 작업 시작");
+
+        registerCallback(future, result -> {
+            System.out.println("비동기 작업 결과:" + result);
+        });
+
+        executorService.shutdown();
+
+    }
+
+    private static void registerCallback(Future<Integer> future, Callback callback) {
+        new Thread(() -> {
+            int result;
+            try {
+                result = future.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+            callback.onComplete(result);
+        }).start();
+    }
+}
---
+++ Java-Concurrency-Programming/src/chapter10/exam04/FutureExample.java
@@ -0,0 +1,33 @@
+package chapter10.exam04;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Future<Integer> future = executorService.submit(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 42;
+        });
+        System.out.println("비동기 작업 시작");
+        try {
+            int result = future.get();
+            System.out.println("비동기 작업결과: " + result);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        executorService.shutdown();
+    }
+}
