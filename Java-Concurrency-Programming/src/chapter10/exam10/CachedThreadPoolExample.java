1ed27cccaaf6dc0643729176d6d928a898e77810
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-11 23:07:17 +0900

    feat : Executors - 유동 및 단일 크기 스레드 풀 생성

---
+++ Java-Concurrency-Programming/src/chapter10/exam10/CachedThreadPoolExample.java
@@ -0,0 +1,19 @@
+package chapter10.exam10;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPoolExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 1; i <= 20; i++) {
+            executorService.submit(() -> {
+                System.out.println("Thread : " + Thread.currentThread().getName());
+            });
+        }
+
+        executorService.shutdown();
+    }
+}
