72ccc0b80896b828beef27ecb4cf211b99d0cc1a
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-12 23:00:58 +0900

    feat : ThreadPoolExecutor - RejectedExecutionHandler 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam11/RejectedExecutionHandlerExample.java
@@ -0,0 +1,52 @@
+package chapter10.exam11;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class RejectedExecutionHandlerExample {
+    public static void main(String[] args) {
+        int corePoolSize = 2;
+        int maxPoolSize = 2;
+        long keepAliveTime = 0L;
+        int workQueueCapacity = 2;
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                corePoolSize,
+                maxPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(workQueueCapacity),
+                new MyRejectedExecutionHandler());
+
+
+        // 작업을 제출
+        for (int i = 1; i <= 5; i++) {
+            final int taskId = i;
+            executor.execute(() -> {
+                System.out.println("Task " + taskId + " is running on thread " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+
+        // 스레드 풀 종료
+        executor.shutdown();
+    }
+}
+
+class MyRejectedExecutionHandler implements RejectedExecutionHandler{
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("태스크가 거부되었습니다.");
+        if(!executor.isShutdown()){
+            executor.getQueue().poll();
+            executor.getQueue().offer(r);
+        }
+    }
+}
