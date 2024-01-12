50c1e0bf0465ec8500fc0a1725502f21e7e9e42f
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-12 22:14:58 +0900

    feat : ThreadPoolExecutor - keepAliveTime 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam11/KeepAliveTimeExample.java
@@ -0,0 +1,39 @@
+package chapter10.exam11;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class KeepAliveTimeExample {
+    public static void main(String[] args) throws InterruptedException {
+
+        int corePoolSize = 2;
+        int maxPoolSize = 4;
+        long keepAliveTime = 1L;
+        BlockingQueue<Runnable> workQueue =  new LinkedBlockingQueue<>(2);
+        int taskNum = 6;
+
+
+        ThreadPoolExecutor executor =
+                new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue);
+
+        for (int i = 0; i < taskNum; i++) {
+            final int taskId = i;
+            executor.execute(()->{
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " 가 태스크" + taskId + " 를 실행하고 있습니다.");
+            });
+        }
+
+        executor.allowCoreThreadTimeOut(true);
+
+
+        Thread.sleep(4000);
+        executor.shutdown();
+    }
+}
