74c7c23b8e69d234be22162e20ba9050ffc3ed3b
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-12 21:59:31 +0900

    feat : ThreadPoolExecutor - corePoolSize&maximumPoolSize 학습

---
+++ Java-Concurrency-Programming/src/chapter10/exam11/PrestartThreadsExample.java
@@ -0,0 +1,40 @@
+package chapter10.exam11;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class PrestartThreadsExample {
+    public static void main(String[] args) {
+
+        int corePoolSize = 2;
+        int maxPoolSize = 4;
+        long keepAliveTime = 0L;
+        BlockingQueue<Runnable> workQueue =  new LinkedBlockingQueue<>();
+        int taskNum = 9;
+
+        ThreadPoolExecutor executor =
+                new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue);
+
+//        executor.prestartCoreThread();
+//        executor.prestartCoreThread();
+
+        executor.prestartAllCoreThreads();
+
+        for (int i = 0; i < taskNum; i++) {
+            final int taskId = i;
+            executor.execute(()->{
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " 가 태스크" + taskId + " 를 실행하고 있습니다.");
+            });
+        }
+
+        executor.shutdown();
+
+    }
+}
---
+++ Java-Concurrency-Programming/src/chapter10/exam11/ThreadPoolExecutorExample.java
@@ -0,0 +1,36 @@
+package chapter10.exam11;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorExample {
+    public static void main(String[] args) {
+
+        int corePoolSize = 2;
+        int maxPoolSize = 4;
+        long keepAliveTime = 0L;
+//        BlockingQueue<Runnable> workQueue =  new LinkedBlockingQueue<>();
+        BlockingQueue<Runnable> workQueue =  new ArrayBlockingQueue<>(4);
+        int taskNum = 9;
+
+        ThreadPoolExecutor executor =
+                new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue);
+
+        for (int i = 0; i < taskNum; i++) {
+            final int taskId = i;
+            executor.execute(()->{
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " 가 태스크" + taskId + " 를 실행하고 있습니다.");
+            });
+        }
+
+        executor.shutdown();
+
+    }
+}
