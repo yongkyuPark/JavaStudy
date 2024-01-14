f3f42d5a33b7d4d3a37a752febf97ac7964abd70
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-01-14 15:00:33 +0900

    feat : 비동기 예외 처리 학습

---
+++ Java-Concurrency-Programming/src/chapter11/exam09/ExceptionallyExample.java
@@ -0,0 +1,25 @@
+package chapter11.exam09;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ExceptionallyExample {
+    public static void main(String[] args) {
+
+        CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(500);
+                throw new IllegalArgumentException("error");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return 10;
+        }).thenApply(r -> r + 20)
+        .exceptionally(e -> {
+            System.out.println("Exceptionb: " + e.getMessage());
+            return -1;
+        });
+
+        System.out.println("result: " + cf.join());
+
+    }
+}
