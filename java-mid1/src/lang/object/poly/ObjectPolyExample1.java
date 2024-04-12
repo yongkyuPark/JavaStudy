4a418c754f00717375d0f803c56eb44b5d64a169
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-12 20:03:21 +0900

    feat : Object 다형성

---
+++ java-mid1/src/lang/object/poly/ObjectPolyExample1.java
@@ -0,0 +1,21 @@
+package lang.object.poly;
+
+public class ObjectPolyExample1 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    private static void action(Object obj) {
+        // 객체에 맞는 다운캐스팅 필요
+        if (obj instanceof Dog dog) {
+            dog.sound();
+        } else if (obj instanceof Car car) {
+            car.move();
+        }
+    }
+}
