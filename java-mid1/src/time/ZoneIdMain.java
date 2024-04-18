636453a1e2564e4925a0ee34ff243e9c7e9ad242
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-18 22:41:40 +0900

    feat : 타임존 - ZonedDateTime

---
+++ java-mid1/src/time/ZoneIdMain.java
@@ -0,0 +1,20 @@
+package time;
+
+import java.time.ZoneId;
+import java.util.Set;
+
+public class ZoneIdMain {
+
+    public static void main(String[] args) {
+        for (String availableZoneId : ZoneId.getAvailableZoneIds()) {
+            ZoneId zoneId = ZoneId.of(availableZoneId);
+            System.out.println(zoneId + " | " + zoneId.getRules());
+        }
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println("ZoneId.systemDefault = " + zoneId);
+
+        ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
+        System.out.println("seoulZoneId = " + seoulZoneId);
+    }
+}
