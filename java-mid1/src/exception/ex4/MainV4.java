58b9eb922a21b7b9d6157f2ec0d9075ec3a6eb85
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-27 13:42:22 +0900

    feat : try-with-resources

--- java-mid1/src/exception/ex4/MainV4.java
+++ java-mid1/src/exception/ex4/MainV4.java
@@ -8,7 +8,8 @@ public class MainV4 {
 
     public static void main(String[] args) {
 
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+        //NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
