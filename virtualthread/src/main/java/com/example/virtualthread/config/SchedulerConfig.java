e7fd17aed2eded24d0ed9438b3c97f416523c242
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-28 15:49:52 +0900

    feat : virtual thread in task scheduling

---
+++ virtualthread/src/main/java/com/example/virtualthread/config/SchedulerConfig.java
@@ -0,0 +1,30 @@
+package com.example.virtualthread.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.task.SimpleAsyncTaskSchedulerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@Slf4j
+public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setThreadNamePrefix("myScheduler-");
+        threadPoolTaskScheduler.setPoolSize(10);
+        // TODO: more settings...
+        return threadPoolTaskScheduler;
+    }
+
+    @Primary
+    @Bean
+    public SimpleAsyncTaskScheduler simpleAsyncTaskScheduler(SimpleAsyncTaskSchedulerBuilder builder) {
+        return builder.build();
+    }
+
+}
