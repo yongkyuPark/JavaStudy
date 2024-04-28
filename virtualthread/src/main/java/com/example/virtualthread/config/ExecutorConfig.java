2adc765a64dfeb9856739ba201dd0f298f90c8bf
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-28 15:45:21 +0900

    feat : virtual thread in task execution

---
+++ virtualthread/src/main/java/com/example/virtualthread/config/ExecutorConfig.java
@@ -0,0 +1,28 @@
+package com.example.virtualthread.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.task.SimpleAsyncTaskExecutorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@Slf4j
+public class ExecutorConfig {
+
+    @Bean
+    public SimpleAsyncTaskExecutor taskExecutor(SimpleAsyncTaskExecutorBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setThreadNamePrefix("myThreadPool-");
+        // TODO: more setting...
+        return threadPoolTaskExecutor;
+    }
+
+}
