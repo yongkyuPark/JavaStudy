6233121125e2efbda56694c948030caf55c405de
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-04-28 14:32:53 +0900

    feat : initial commit

---
+++ virtualthread/src/main/java/com/example/virtualthread/VirtualthreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualthreadApplication.class, args);
+	}
+
+}
