e907dcc05b5df85c129151904978a739cb5fe02d
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-10 23:17:31 +0900

    feat : 제네릭 적용

---
+++ java-mid2/src/generic/ex1/BoxMain3.java
@@ -0,0 +1,25 @@
+package generic.ex1;
+
+public class BoxMain3 {
+
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<Integer>(); // 생성 시점에 T의 타입 결정
+        integerBox.set(10);
+        Integer integer = integerBox.get(); // Integer 타입 반환 (캐스팅 X)
+        System.out.println("integer = " + integer);
+
+        GenericBox<String> stringBox = new GenericBox<String>();
+        stringBox.set("hello");
+        String str = stringBox.get();
+        System.out.println("str = " + str);
+        
+        // 원하는 모든 타입 사용 가능
+        GenericBox<Double> doubleBox = new GenericBox<>();
+        doubleBox.set(10.5);
+        Double doubleValue = doubleBox.get();
+        System.out.println("doubleValue = " + doubleValue);
+
+        // 타입 추론: 생성하는 제네릭 타입 생략 가능
+        GenericBox<Integer> integerBox2 = new GenericBox<>();
+    }
+}
