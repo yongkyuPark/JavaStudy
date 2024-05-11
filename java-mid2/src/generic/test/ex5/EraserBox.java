c28c545ba3f95f295301f5645dc8f3ac7b423327
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-11 21:17:40 +0900

    feat : 타입 이레이저

---
+++ java-mid2/src/generic/test/ex5/EraserBox.java
@@ -0,0 +1,13 @@
+package generic.test.ex5;
+
+public class EraserBox<T> {
+
+    public boolean instanceCheck(Object param) {
+        //return param instanceof T;
+        return false;
+    }
+
+    public void create() {
+        //return new T();
+    }
+}
