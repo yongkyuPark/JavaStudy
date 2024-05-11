2371fc42e94209ba2d42085acd9e9dc1f0c24234
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-11 13:27:10 +0900

    feat : 타입 매개변수 제한 - 시작

--- java-mid2/src/generic/test/ex2/PairTest.java
+++ java-mid2/src/generic/test/ex2/PairTest.java
@@ -10,7 +10,7 @@ public class PairTest {
         System.out.println(pair1.getFirst());
         System.out.println(pair1.getSecond());
         System.out.println("pair1 = " + pair1);
-        
+
         Pair<String, String> pair2 = new Pair<>();
         pair2.setFirst("key");
         pair2.setSecond("value");
