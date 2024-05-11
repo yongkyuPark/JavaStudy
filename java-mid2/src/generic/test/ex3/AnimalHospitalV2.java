80095b58f575ffc7b481f833280697efbc475122
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-11 14:00:54 +0900

    feat : 타입 매개변수 제한 - 제네릭 도입과 실패

---
+++ java-mid2/src/generic/test/ex3/AnimalHospitalV2.java
@@ -0,0 +1,24 @@
+package generic.test.ex3;
+
+public class AnimalHospitalV2<T> {
+
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public void checkup() {
+        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용
+        animal.toString();
+
+        //System.out.println("동물 이름: " + animal.getName());
+        //System.out.println("동물 크기: " + animal.getSize());
+        //animal.sound();
+    }
+
+    public T bigger(T target) {
+        //return animal.getSize() > target.getSize() ? animal : target;
+        return null;
+    }
+}
