d4ae136a117428b24bfe420c0c2f8e4464447401
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-12 14:02:06 +0900

    feat : 직접 구현하는 배열 리스트 - 동적 배열

---
+++ java-mid2/src/collection/array/MyArrayListV2.java
@@ -0,0 +1,64 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV2 {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV2() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV2(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        // 코드 추가
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    // 코드 추가
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        //배열을 새로 만들고, 기존 배열을 새로운 배열에 복사
+        elementData = Arrays.copyOf(elementData, newCapacity);;
+    }
+
+    public Object get(int index) {
+        return elementData[index];
+    }
+
+    public Object set(int index, Object element) {
+        Object oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) +
+                " size=" + size + ", capacity=" + elementData.length;
+    }
+}
