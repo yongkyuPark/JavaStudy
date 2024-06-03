5542903a11ebbbc5b93002d11ab548ab99657d2d
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-06-03 20:43:29 +0900

    feat : 자바의 hashCode()

---
+++ java-mid2/src/collection/set/member/Member.java
@@ -0,0 +1,36 @@
+package collection.set.member;
+
+import java.util.Objects;
+
+public class Member {
+
+    private String id;
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Member member = (Member) object;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
