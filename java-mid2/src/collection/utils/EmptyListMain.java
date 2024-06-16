9ba0e69f0aafb805ac7658446a7f251d91989893
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-06-16 14:15:15 +0900

    feat : 컬렉션 유틸

---
+++ java-mid2/src/collection/utils/EmptyListMain.java
@@ -0,0 +1,22 @@
+package collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EmptyListMain {
+
+    public static void main(String[] args) {
+        //빈 가변 리스트 생성
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+        //빈 불변 리스트 생성
+        List<Integer> list3 = Collections.emptyList();
+        List<Integer> list4 = List.of();
+
+        System.out.println("list3.getClass() = " + list3.getClass());
+        System.out.println("list4.getClass() = " + list4.getClass());
+    }
+}
