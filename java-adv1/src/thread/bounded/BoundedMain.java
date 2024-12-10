8f908beaf41907e7853bb74e81ba5fb0ac81b988
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-10 21:57:13 +0900

    feat : BlockingQueue - 예제6

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -14,11 +14,12 @@ public class BoundedMain {
         //BoundedQueue queue = new BoundedQueueV2(2);
         //BoundedQueue queue = new BoundedQueueV3(2);
         //BoundedQueue queue = new BoundedQueueV4(2);
-        BoundedQueue queue = new BoundedQueueV5(2);
+        //BoundedQueue queue = new BoundedQueueV5(2);
+        BoundedQueue queue = new BoundedQueueV6_1(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
-        //ProducerFirst(queue); // 생산자 먼저 실행
-        consumerFirst(queue); // 소비자 먼저 실행
+        ProducerFirst(queue); // 생산자 먼저 실행
+        //consumerFirst(queue); // 소비자 먼저 실행
     }
 
     private static void consumerFirst(BoundedQueue queue) {
