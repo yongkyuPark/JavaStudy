27094616a31beaff096fcd3a2597e402dec35534
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-11 21:48:34 +0900

    feat : 원자적 연산 - volatile, synchronized

--- java-adv1/src/thread/cas/increment/IncrementThreadMain.java
+++ java-adv1/src/thread/cas/increment/IncrementThreadMain.java
@@ -11,6 +11,8 @@ public class IncrementThreadMain {
 
     public static void main(String[] args) throws InterruptedException {
         test(new BasicInteger());
+        test(new VolatileInteger());
+        test(new SyncInteger());
     }
 
     private static void test(IncrementInteger incrementInteger) throws InterruptedException {
