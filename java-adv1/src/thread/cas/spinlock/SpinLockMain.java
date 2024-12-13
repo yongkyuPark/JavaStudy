360204c6dd49a8c9ddd6c78ddce7b256becc2f8e
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-13 22:34:46 +0900

    feat : CAS 락 구현2

--- java-adv1/src/thread/cas/spinlock/SpinLockMain.java
+++ java-adv1/src/thread/cas/spinlock/SpinLockMain.java
@@ -1,11 +1,13 @@
 package thread.cas.spinlock;
 
 import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
 
 public class SpinLockMain {
 
     public static void main(String[] args) {
-        SpinLockBad spinLock = new SpinLockBad();
+        //SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
 
         Runnable task = new Runnable() {
             @Override
@@ -14,6 +16,7 @@ public class SpinLockMain {
                 try {
                     // critical section
                     log("비즈니스 로직 실행");
+                    sleep(1); // 오래 걸리는 로직에서 스핀 락 사용X
                 } finally {
                     spinLock.unlock();
                 }
