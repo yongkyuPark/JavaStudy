4ca250bbfc8946521c12b7b3105611f7d05207fd
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-23 00:25:48 +0900

    feat : 회원 관리 예제3 - DataStream

--- java-adv2/src/io/member/MemberConsoleMain.java
+++ java-adv2/src/io/member/MemberConsoleMain.java
@@ -1,5 +1,6 @@
 package io.member;
 
+import io.member.impl.DataMemberRepository;
 import io.member.impl.FileMemberRepository;
 import io.member.impl.MemoryMemberRepository;
 
@@ -9,7 +10,8 @@ import java.util.Scanner;
 public class MemberConsoleMain {
 
     //private static final MemberRepository repository = new MemoryMemberRepository();
-    private static final MemberRepository repository = new FileMemberRepository();
+    //private static final MemberRepository repository = new FileMemberRepository();
+    private static final MemberRepository repository = new DataMemberRepository();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
