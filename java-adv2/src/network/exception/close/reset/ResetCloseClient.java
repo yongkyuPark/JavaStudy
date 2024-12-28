12813de0d60c42662c6b2b9327dbbefdea1abc25
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-28 20:29:12 +0900

    feat : 네트워크 예외4 - 강제 종료

---
+++ java-adv2/src/network/exception/close/reset/ResetCloseClient.java
@@ -0,0 +1,41 @@
+package network.exception.close.reset;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+import static util.MyLogger.log;
+
+public class ResetCloseClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket("localhost", 12345);
+        log("소켓 연결: " + socket);
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+
+        // client <- server: FIN
+        Thread.sleep(1000); // 서버가 close() 호출할 때 까지 잠시 대기
+
+        // client -> server: PUSH[1]
+        output.write(1);
+
+        // client <- server: RST
+        Thread.sleep(1000); // RST 메시지 전송 대기
+
+        try {
+            int read = input.read();
+            System.out.println("read = " + read);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            output.write(1);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
