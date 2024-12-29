440c3f5ad98fa9f5e7568c17067ec99e775572d3
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-12-29 21:18:26 +0900

    feat : 채팅 프로그램 - 서버2

--- java-adv2/src/chat/server/ServerMain.java
+++ java-adv2/src/chat/server/ServerMain.java
@@ -10,7 +10,8 @@ public class ServerMain {
         SessionManager sessionManager = new SessionManager();
 
         // CommandManager 점진적으로 변경 예정
-        CommandManagerV1 commandManager = new CommandManagerV1(sessionManager);
+        //CommandManager commandManager = new CommandManagerV1(sessionManager);
+        CommandManager commandManager = new CommandManagerV2(sessionManager);
 
         Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
