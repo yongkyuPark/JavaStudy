c8e8a2dad209b276e3f54b8d793b6f5f89fd3444
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-01-02 22:13:13 +0900

    feat : 생성자 탐색과 객체 생성

--- java-adv2/src/reflection/data/BasicData.java
+++ java-adv2/src/reflection/data/BasicData.java
@@ -9,7 +9,7 @@ public class BasicData {
         System.out.println("BasicData.BasicData");
     }
 
-    public BasicData(String data) {
+    private BasicData(String data) {
         System.out.println("BasicData.BasicData: " + data);
     }
 
