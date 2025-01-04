23398fe09a483c642f176a2f61148ec3b7f75308
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-01-04 16:38:06 +0900

    feat : 애노테이션 활용 - 검증기

---
+++ java-adv2/src/annotation/validator/ValidatorV2Main.java
@@ -0,0 +1,27 @@
+package annotation.validator;
+
+import static util.MyLogger.log;
+
+public class ValidatorV2Main {
+
+    public static void main(String[] args) {
+        User user = new User("user1", 0);
+        Team team = new Team("teamA", 0);
+
+        try {
+            log("== user 검증 ==");
+            Validator.validate(user);
+        } catch (Exception e) {
+            log(e);
+        }
+
+        try {
+            log("== team 검증 ==");
+            Validator.validate(team);
+        } catch (Exception e) {
+            log(e);
+        }
+    }
+
+
+}
