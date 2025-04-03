6bd56ad99dd0c2693b84348cf5c2c8d9eaa6982d
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-04-03 21:07:43 +0900

    feat : 특화 함수형 인터페이스

---
+++ java-adv3/src/lambda/lambda4/OperatorMain.java
@@ -0,0 +1,24 @@
+package lambda.lambda4;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class OperatorMain {
+
+    public static void main(String[] args) {
+        // UnaryOperator
+        Function<Integer, Integer> square1 = x -> x * x;
+        UnaryOperator<Integer> square2 = x -> x * x;
+        System.out.println("square1: " + square1.apply(5));
+        System.out.println("square2: " + square2.apply(5));
+
+        // BinaryOperator
+        BiFunction<Integer, Integer, Integer> addition1 = (a, b) -> a + b;
+        BinaryOperator<Integer> addition2 = (a, b) -> a + b;
+
+        System.out.println("addition1: " + addition1.apply(1, 2));
+        System.out.println("addition2: " + addition2.apply(1, 2));
+    }
+}
