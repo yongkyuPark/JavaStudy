08384b2bb26b3ec905b08c8c3aec68200e10dcb7
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-04-07 19:44:44 +0900

    feat : 필터 만들기

---
+++ java-adv3/src/lambda/lambda5/filter/FilterMainV5.java
@@ -0,0 +1,20 @@
+package lambda.lambda5.filter;
+
+import java.util.List;
+
+public class FilterMainV5 {
+
+    public static void main(String[] args) {
+        // 숫자 사용 필터
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numberResult = GenericFilter.filter(numbers, n -> n % 2 == 0);
+        System.out.println("numberResult = " + numberResult);
+
+        // 문자 사용 필터
+        List<String> strings = List.of("A", "BB", "CCC");
+        List<String> stringsResult = GenericFilter.filter(strings, s -> s.length() >= 2);
+        System.out.println("stringsResult = " + stringsResult);
+
+
+    }
+}
