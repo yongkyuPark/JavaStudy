19c1543d87c2738bd8faf73a97400fe204245804
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-04-14 21:37:32 +0900

    feat : 메서드 참조 - 활용

---
+++ java-adv3/src/methodref/MethodRef5.java
@@ -0,0 +1,28 @@
+package methodref;
+
+import lambda.lambda5.mystream.MyStreamV3;
+
+import java.util.List;
+
+public class MethodRef5 {
+
+    public static void main(String[] args) {
+        List<Person> personList = List.of(
+                new Person("Kim"),
+                new Person("Park"),
+                new Person("Lee")
+        );
+
+        List<String> result1 = MyStreamV3.of(personList)
+                .map(person -> person.introduce())
+                .map(str -> str.toUpperCase())
+                .toList();
+        System.out.println("result1 = " + result1);
+
+        List<String> result2 = MyStreamV3.of(personList)
+                .map(Person::introduce)
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("result2 = " + result2);
+    }
+}
