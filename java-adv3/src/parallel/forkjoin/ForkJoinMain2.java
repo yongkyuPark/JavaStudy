f28183f93482341e8c81d3e9861f99b492ebd79d
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-04-26 23:28:55 +0900

    feat : Fork/Join 프레임워크

---
+++ java-adv3/src/parallel/forkjoin/ForkJoinMain2.java
@@ -0,0 +1,25 @@
+package parallel.forkjoin;
+
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+
+import static util.MyLogger.log;
+
+public class ForkJoinMain2 {
+
+    public static void main(String[] args) {
+        int processorCount = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool commonPool = ForkJoinPool.commonPool();
+        log("processorCount = " + processorCount + ", commonPool = " + commonPool.getParallelism());
+
+        List<Integer> data = IntStream.rangeClosed(1, 8)
+                .boxed()
+                .toList();
+
+        log("[생성] " + data);
+        SumTask task = new SumTask(data);
+        Integer result = task.invoke(); // 공용 풀 사용
+        log("최종 결과: " + result);
+    }
+}
