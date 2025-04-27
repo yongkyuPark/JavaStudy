c4ea6e54b7f59527ce183470bb4546176613a197
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-04-27 12:59:04 +0900

    feat : 자바 병렬 스트림

---
+++ java-adv3/src/parallel/ParallelMain4.java
@@ -0,0 +1,26 @@
+package parallel;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+
+import static util.MyLogger.log;
+
+public class ParallelMain4 {
+
+    public static void main(String[] args) {
+
+        int processorCount = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool commonPool = ForkJoinPool.commonPool();
+        log("processorCount = " + processorCount + ", commonPool = " + commonPool.getParallelism());
+
+        long startTime = System.currentTimeMillis();
+
+        int sum = IntStream.rangeClosed(1, 8)
+                .parallel()
+                .map(HeavyJob::heavyTask)
+                .reduce(0, (a, b) -> a + b);
+
+        long endTime = System.currentTimeMillis();
+        log("time: " + (endTime - startTime) + "ms, sum: " + sum);
+    }
+}
